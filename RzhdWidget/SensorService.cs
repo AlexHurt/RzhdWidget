c14466e81f4d217cc4cd835226df23bf4b97065d
AlexHurt <alexey23081985@gmail.com>
2019-09-28 18:00:26 +0300

    Added location service

---
+++ RzhdWidget/SensorService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Locations;
+using Android.OS;
+using Android.Support.V4.Content;
+using Android.Widget;
+
+namespace RzhdWidget
+{
+    internal class SensorService : Service, ILocationListener
+    {
+        private LocationManager _locationManager;
+        private string mProvider;
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            _locationManager = (LocationManager)GetSystemService(LocationService);
+            mProvider = _locationManager.GetBestProvider(new Criteria(), false);
+            if (!String.IsNullOrEmpty(mProvider))
+            {
+                if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) !=
+                    Permission.Granted &&
+                    ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) !=
+                    Permission.Granted)
+                {
+                    StopSelf();
+                    return;
+                }
+
+                var location = _locationManager.GetLastKnownLocation(mProvider);
+                _locationManager.RequestLocationUpdates(mProvider, 15000, 1, this);
+                if (location != null)
+                    OnLocationChanged(location);
+                else
+                    Toast.MakeText(this, "No Location Provider Found", ToastLength.Short).Show();
+            }
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ RzhdWidget/WidgetControl/MainAppWidgetProvider.cs
@@ -0,0 +1,18 @@
+﻿using Android.Appwidget;
+using Android.Content;
+
+namespace RzhdWidget.WidgetControl
+{
+    class MainAppWidgetProvider : AppWidgetProvider
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            
+        }
+
+        public override void OnUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+        {
+
+        }
+    }
+}
